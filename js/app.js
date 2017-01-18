e35f9900f23e306b01f7afc4652ca50821a4c5cb
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-18 23:41:52 +0200

    table rendering

--- js/app.js
+++ js/app.js
@@ -12,3 +12,25 @@ function getInfo(url) {
 
 var lastDaysCampers = getInfo(lastDaysUrl);
 var allTimeCampers = getInfo(allTimeUrl);
+
+function BoardList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number, index) =>
+
+            <tr>
+                <td className='number'>{index + 1}</td>
+                <td className='username'><img src={number['img']}/>{number['username']}</td>
+                <td className='alltimePoints'>{number['alltime']}</td>
+                <td className='recentPoints'>{number['recent']}</td>
+            </tr>
+    );
+    return (
+        <table className='container'>{listItems}</table>
+    );
+}
+
+
+ReactDOM.render(
+    <BoardList numbers={lastDaysCampers}/>,
+    document.getElementById('root')
+)
